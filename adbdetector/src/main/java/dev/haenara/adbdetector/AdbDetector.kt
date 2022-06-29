f8b2b10b6799be7ca9448379c5c3ecf0dabd5bb5
insanity <bokuhe@gmail.com>
2022-06-29 19:21:49 +0900

    refactor: refactor and cleanup lib code

--- adbdetector/src/main/java/dev/haenara/adbdetector/AdbDetector.kt
+++ adbdetector/src/main/java/dev/haenara/adbdetector/AdbDetector.kt
@@ -1,9 +1,7 @@
 package dev.haenara.adbdetector
 
-import android.annotation.TargetApi
 import android.content.Context
 import android.content.IntentFilter
-import android.os.Build
 import android.provider.Settings
 
 /**
@@ -11,69 +9,72 @@ import android.provider.Settings
  * To check it dynamically, use a register a BroadcastReceiver.
  * To check it statically, use checkUsbDebuggingMode function.
  */
-class AdbDetector(private val mContext : Context) {
+@Suppress("MemberVisibilityCanBePrivate")
+class AdbDetector(private val mContext: Context) {
     /**
      * Register an ADB-detect receiver,so that checks
      * if USB Debugging is enabled and usb is connected dynamically.
      */
     fun registerAdbDetectReceiver(listener: OnAdbUsbListener? = null) {
-        // USB 연결을 감지하는 필터를 등록
+        // Register USB connect filter.
+        unregisterAdbDetectReceiver()
         val filter = IntentFilter()
         filter.addAction("android.hardware.usb.action.USB_STATE")
-        val receiver = UsbDebugDetectReceiver(listener)
+        receiver = UsbDebugDetectReceiver(listener)
         mContext.registerReceiver(receiver, filter)
-        lastListener = receiver
+    }
+
+    fun registerAdbDetectReceiver(
+        connected: ((context: Context) -> Unit)?,
+        disconnected: ((context: Context) -> Unit)?
+    ) {
+        registerAdbDetectReceiver(object : OnAdbUsbListener {
+            override fun onAdbUsbConnected(context: Context) {
+                connected?.let { connected(context) }
+            }
+
+            override fun onAdbUsbDisconnected(context: Context) {
+                disconnected?.let { disconnected(context) }
+            }
+        })
     }
 
     /**
      * Unregister the last Adb detect receiver.
      */
     fun unregisterAdbDetectReceiver() {
-        // USB 연결을 감지하는 필터를 등록
-        lastListener?.let{
-            mContext.unregisterReceiver(lastListener)
+        // Unregister USB connect filter.
+        receiver?.let {
+            mContext.unregisterReceiver(receiver)
         }
     }
 
 
     /**
-    * Check if USB Debugging is enabled and usb is connected statically.
-    */
-    fun checkUsbDebuggingMode(): Boolean {
-        return isDebugEnable() and isUsbConnected()
-    }
+     * Check if USB Debugging is enabled and usb is connected statically.
+     */
+    fun checkUsbDebuggingMode(): Boolean = isDebugEnabled() and isUsbConnected()
 
     /**
      * Check if USB Debugging is enabled.
      */
-    @TargetApi(17)
-    fun isDebugEnable(): Boolean {
-        return when {
-            Build.VERSION.SDK_INT == Build.VERSION_CODES.JELLY_BEAN -> Settings.Secure.getInt(
-                mContext.contentResolver,
-                Settings.Secure.ADB_ENABLED, 0
-            ) != 0
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 -> Settings.Secure.getInt(
-                mContext.contentResolver,
-                Settings.Global.ADB_ENABLED, 0
-            ) != 0
-            else -> false
-        }
-    }
+    fun isDebugEnabled(): Boolean =
+        Settings.Secure.getInt(
+            mContext.contentResolver, Settings.Global.ADB_ENABLED, 0
+        ) != 0
 
     /**
      * Check if USB is connected statically.
      */
-    private fun isUsbConnected(): Boolean {
+    fun isUsbConnected(): Boolean {
         val intent = mContext.registerReceiver(
             null,
             IntentFilter("android.hardware.usb.action.USB_STATE")
         )
-        return intent != null && intent.extras != null &&
-                intent.extras!!.getBoolean("connected")
+        return intent?.extras?.getBoolean("connected") ?: false
     }
 
     companion object {
-        var lastListener : UsbDebugDetectReceiver? = null
+        private var receiver: UsbDebugDetectReceiver? = null
     }
 }
--- adbdetector/src/main/java/dev/haenara/adbdetector/EmptyActivity.kt
+++ adbdetector/src/main/java/dev/haenara/adbdetector/EmptyActivity.kt
@@ -10,16 +10,15 @@ import kotlin.system.exitProcess
  * that kills application process.
  */
 class EmptyActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_empty)
 
         AlertDialog.Builder(this)
             .setCancelable(false)
-            .setTitle(R.string.adb_detect_error_title)
-            .setMessage(R.string.adb_detect_error_message)
-            .setPositiveButton(R.string.adb_detect_error_confirm) { _, _->
+            .setTitle(R.string.adb_detect_default_error_title)
+            .setMessage(R.string.adb_detect_default_error_message)
+            .setPositiveButton(R.string.adb_detect_default_error_confirm) { _, _ ->
                 android.os.Process.killProcess(android.os.Process.myPid())
                 exitProcess(-1)
             }
